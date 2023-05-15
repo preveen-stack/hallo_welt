c642a11d8de80f29030016e71b66737f8ec83e04
Preveen P <31464911+preveen-stack@users.noreply.github.com>
2023-05-15 15:46:10 +0530

    Create hallo_welt.c

---
+++ hallo_welt.c
@@ -0,0 +1,9 @@
+#include "shlusselworter.h"
+
+ganzzahl haupt_funktion() {
+    // Ausgabe des Begrüßungstextes
+    drucken("Hallo, Welt!\n");
+
+    // Rückgabewert 0 signalisiert erfolgreiche Ausführung
+    rueckgabe 0;
+}
